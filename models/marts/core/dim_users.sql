57b4311e60f10b7257d4e4885b5652abdcfe0943
blazquezmark97@gmail.com <blazquezmark97@gmail.com>
2022-12-08 22:37:33 +0000

    feat: implementafo snapshot a tabla addresses por lo que la dim_users  ahora mantiene el historico

--- models/marts/core/dim_users.sql
+++ models/marts/core/dim_users.sql
@@ -1,30 +1,28 @@
-WITH stg_sql_server_dbo_users AS (
+WITH int_users AS (
     SELECT * 
-    FROM {{ ref('stg_sql_server_dbo_users') }}
-    ),
-stg_sql_server_dbo_addresses AS (
-    SELECT * 
-    FROM {{ ref('stg_sql_server_dbo_addresses') }}
+    FROM {{ ref('int_users') }}
     ),
 
-dim_users AS (
+fact_users AS (
     SELECT
-        md5(USER_ID) as user_id,
-        USER_ID as nk_user_id,
+    
+        user_id,
+        nk_user_id,
         FIRST_NAME,
         LAST_NAME,
         PHONE_NUMBER,
         CREATED_AT_UTC,
         UPDATED_AT_UTC,
         EMAIL,
-        ADDRESS_NUMBER,
-        ADDRESS_NAME,
-        ZIPCODE
+        address_number,
+        address_name ,
+        ZIPCODE,
         COUNTRY,
-        STATE
-    from stg_sql_server_dbo_users join stg_sql_server_dbo_addresses 
-    on stg_sql_server_dbo_users.address_id=stg_sql_server_dbo_addresses.address_id
+        STATE,
+        dato_no_valido_desde
+
+    FROM int_users 
 
     )
 
-SELECT * FROM dim_users
+SELECT * FROM fact_users
