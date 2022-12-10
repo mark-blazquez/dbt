fe291e501319b00a7e55c61002606ee07905dc39
blazquezmark97@gmail.com <blazquezmark97@gmail.com>
2022-12-10 22:18:26 +0000

    feat: creado dim_productos como incremental

--- models/marts/core/dim_products.sql
+++ models/marts/core/dim_products.sql
@@ -1,3 +1,8 @@
+{{ config(
+    materialized='incremental'
+    ) 
+}}
+
 WITH stg_sql_server_dbo_products AS (
     SELECT * 
     FROM {{ ref('stg_sql_server_dbo_products') }}
@@ -10,8 +15,15 @@ dim_products AS (
         PRICE,
         NAME,
         INVENTORY,
+        _FIVETRAN_SYNCED ,
         dato_no_valido_desde
     FROM stg_sql_server_dbo_products
     )
 
 SELECT * FROM dim_products
+
+{% if is_incremental() %}
+
+  where _fivetran_synced > (select max(_fivetran_synced) from dim_products)
+
+{% endif %}
