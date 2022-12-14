0b23812e8e3cefe3dba670987fe3cfe0e7ef1383
blazquezmark97@gmail.com <blazquezmark97@gmail.com>
2022-12-14 12:31:42 +0000

    feat: hecho a mi forma el datamart para el caso de producto

--- models/marts/producto/producto.sql
+++ models/marts/producto/producto.sql
@@ -9,28 +9,35 @@ fact_events AS (
 datamart_producto AS (
     SELECT
 
-        SESSION_ID,
+        session_id,
         fact_events.user_id,
         first_name,
         email,
         min(fact_events.CREATED_AT_UTC) as first_event_time_utc,
         max(fact_events.CREATED_AT_UTC) as last_event_time_utc,
-        DATEDIFF (minute,  min(fact_events.CREATED_AT_UTC) , max(fact_events.CREATED_AT_UTC) ) as session_lenght_minutes
-        --as page_view
-        --as ass_to_cart,
-        --as checkout
-        --as package_shipped
+        DATEDIFF (minute,  min(fact_events.CREATED_AT_UTC) , max(fact_events.CREATED_AT_UTC) ) as session_lenght_minutes,
+        CASE
+            WHEN event_type='page_view' THEN count(event_type='page_view')
+        END as page_view  ,
+        CASE
+            WHEN event_type='add_to_cart' THEN count(event_type='add_to_cart')
+        END as add_to_cart   ,   
+        CASE
+            WHEN event_type='checkout' THEN count(event_type='checkout')
+        END as checkout  ,    
+        CASE
+            WHEN event_type='package_shipped' THEN count(event_type='package_shipped')
+        END as package_shipped     
 
     FROM fact_events join dim_users on fact_events.user_id=dim_users.user_id
     group by 
-
-        SESSION_ID,
+        session_id,
+        event_type,
         fact_events.user_id,
         first_name,
-        email,
-        event_type
-
+        email
     order by first_name
+
 
     )
 
